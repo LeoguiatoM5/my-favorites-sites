748458a6d0ef9635628982403a7533c53430ec7f
LeoguiatoM5 <leonardoguiato99@gmail.com>
2023-01-22 15:19:09 -0300

    Permitir cadastro de site

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.myfavoritessites {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.myfavoritessites to javafx.fxml;
-    exports com.example.myfavoritessites;
-}
